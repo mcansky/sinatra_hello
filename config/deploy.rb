379f9e7c83e15c4dfe2d02123998340a89eae4d6
Thomas Riboulet <riboulet@gmail.com>
2014-03-20 21:40:13 +0000

    wrong root

--- config/deploy.rb
+++ config/deploy.rb
@@ -36,7 +36,7 @@ namespace :deploy do
     run "mkdir -p #{shared_path}/pids"
     run "mkdir -p #{shared_path}/logs"
     run "mkdir -p #{shared_path}/sockets"
-    run "mkdir -p #{shared_path}/releases"
+    run "mkdir -p #{deploy_to}/releases"
   end
 
   task :symlink_config, roles: :app do

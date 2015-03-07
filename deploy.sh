4572d071943883d47a060eac52bb0252761a2a7e
Thomas Riboulet <riboulet@gmail.com>
2015-03-07 00:28:06 +0000

    update configs

--- deploy.sh
+++ deploy.sh
@@ -11,7 +11,7 @@ EB_BUCKET=sinatra-hello-bucket
 DOCKERRUN_FILE=$SHA1-Dockerrun.aws.json
 sed "s/<TAG>/$SHA1/" < Dockerrun.aws.json.template > $DOCKERRUN_FILE
 aws s3 cp $DOCKERRUN_FILE s3://$EB_BUCKET/$DOCKERRUN_FILE
-aws elasticbeanstalk create-application-version --application-name sinatra-hello \
+aws elasticbeanstalk create-application-version --application-name sinatra_hello_eb1 \
   --version-label $SHA1 --source-bundle S3Bucket=$EB_BUCKET,S3Key=$DOCKERRUN_FILE
 
 # Update Elastic Beanstalk environment to new version

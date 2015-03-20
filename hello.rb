23d8238c5f18fd60a3094453d02cb3f789fadb89
Thomas Riboulet <riboulet@gmail.com>
2015-03-20 21:09:32 +0000

    remove line

--- hello.rb
+++ hello.rb
@@ -3,7 +3,6 @@ require 'haml'
 require 'sidekiq'
 require './workers/test_job'
 
-
 Sidekiq.configure_server do |config|
   config.redis = { url: ENV['REDIS_URL'] || 'redis://localhost:6379/1' }
 end

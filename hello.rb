13e60b8f6a07369b67a9ce81443dc06ff8576874
Thomas Riboulet <riboulet@gmail.com>
2019-01-08 13:21:59 +0100

    simplify hello world to the last bit

--- hello.rb
+++ hello.rb
@@ -4,25 +4,10 @@ require 'redis'
 require 'sidekiq/api'
 require 'haml'
 
-$redis = Redis.new(url: ENV['REDIS_URL'])
-
 class SimpleApp < Sinatra::Base
 
   get '/' do
-    SinatraWorker.perform_async(msg="beanstalk was pulled")
-    haml "%h3 Hello THE BEANSTALK AWESOME World!"
-  end
-
-  get '/stats' do
-    stats = Sidekiq::Stats.new
-    @failed = stats.failed
-    @processed = stats.processed
-    @messages = $redis.lrange('sinkiq-example-messages', 0, -1)
-    erb :index
-  end
-
-  get '/mu-c9478619-b3ea1fef-e218a7ee-09081759' do
-    '42'
+    haml "%h3 Hello World!"
   end
 
 end
